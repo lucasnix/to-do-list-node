3611deed857227002000f12ff1b13adbde4df220
lucasnix <lucasniix@gmail.com>
2022-02-11 20:43:25 -0300

    commit-mensagens-de-alerta-OK

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -1,10 +1,22 @@
 const { get } = require("express/lib/response");
 const Task = require("../models/Task");
 
+let message = "";
+let type = "";
+
 const getAllTasks = async (req, res) => {
   try {
+    setTimeout(() => {
+      message = "";
+    }, 1000);
     const tasksList = await Task.find();
-    return res.render("index", { tasksList, task: null, taskDelete:null });
+    return res.render("index", {
+      tasksList,
+      task: null,
+      taskDelete: null,
+      message,
+      type,
+    });
   } catch (err) {
     res.status(500).send({ error: err.message });
   }
@@ -14,10 +26,14 @@ const createTask = async (req, res) => {
   const task = req.body;
 
   if (!task.task) {
+    message = "insira um texto, antes de adicionar a tarefa!";
+    type = "danger";
     return res.redirect("/");
   }
   try {
     await Task.create(task);
+    message = "Tarefa criada com sucesso";
+    type = "success";
     return res.redirect("/");
   } catch (err) {
     res.status(500).send({ error: err.message });
@@ -27,12 +43,12 @@ const createTask = async (req, res) => {
 const getById = async (req, res) => {
   try {
     const tasksList = await Task.find();
-    if (req.params.method == "updade"){
-      const task  = await Task.findOne({ _id: req.params.id });
-      res.render("index", { task, taskDelete: null, tasksList });
+    if (req.params.method == "updade") {
+      const task = await Task.findOne({ _id: req.params.id });
+      res.render("index", { task, taskDelete: null, tasksList, message, type });
     } else {
-      const taskDelete  = await Task.findOne({ _id: req.params.id });
-      res.render("index", { task: null, taskDelete, tasksList });
+      const taskDelete = await Task.findOne({ _id: req.params.id });
+      res.render("index", { task: null, taskDelete, tasksList, message, type });
     }
   } catch (err) {
     res.status(500).send({ error: err.message });
@@ -42,7 +58,9 @@ const getById = async (req, res) => {
 const updateOneTask = async (req, res) => {
   try {
     const task = req.body;
-    await Task.updateOne ({ _id: req.params.id }, task);
+    await Task.updateOne({ _id: req.params.id }, task);
+    message = "Tarefa atualizada com sucesso";
+    type = "success";
     res.redirect("/");
   } catch (err) {
     res.status(500).send({ error: err.message });
@@ -51,13 +69,14 @@ const updateOneTask = async (req, res) => {
 
 const deleteOneTask = async (req, res) => {
   try {
-    await Task.deleteOne({ _id: req.params.id})
+    await Task.deleteOne({ _id: req.params.id });
+    message = "Tarefa apagada com sucesso";
+    type = "success";
     res.redirect("/");
   } catch (err) {
     res.status(500).send({ error: err.message });
   }
-
-}
+};
 
 module.exports = {
   getAllTasks,
